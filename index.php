0f74253a63e11e4d51afc6dd11b86eefe74f524e
jwhistine <justinwhistine@gmail.com>
2015-05-02 08:42:15 -0600

    added homepage

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Homepage</title> 
+        <link rel="stylesheet" type="text/css" href="imageDiv.css">
+        <link rel="stylesheet" type="text/css" href="menu.css">
+    </head>
+    <body>
+        <div class="main">
+            <header>
+                <h1 class="h1">Justin Whistine</h1>
+            </header>
+            <hr>
+            <br>
+            <!-- drop down menu -->
+            <div id='cssmenu'>
+            <ul>
+                <li><a href='index.php'><span>Home</span></a></li>
+                <li><a href="about.html"><span>About</span></a></li>
+                <li><a href="assignments.html"><span>Assignments</span></a></li>
+                <li class='has-sub'><a href='#'><span>Contact</span></a>
+                <ul>
+                    <li><a href='#'><span>whi12007@byui.edu</span></a></li>
+                </ul>
+                </li>
+            </ul>
+            </div>
+            <div id="container">
+            <br>
+            <br>
+            <!-- Each image is 350px by 233px --> 
+            <div class="photobanner">
+                <img class="first" src="spt.jpg" alt="">
+                <img src="ct.jpg" alt="">
+                <img src="atlT.jpg" alt="">
+                <img src="ifT.jpg" alt="">
+                <img src="rt.jpg" alt="">
+                <img src="dct.jpg" alt="">
+                <img src="sct.jpg" alt="">
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
